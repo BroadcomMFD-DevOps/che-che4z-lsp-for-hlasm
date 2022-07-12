3774ba257183e03abb368a9912c4bdef5827e4ab
Jiri Mosinger <jiri.mosinger@broadcom.com>
2022-07-12 15:00:37 +0200

    Regex

--- parser_library/src/workspaces/wildcard.cpp
+++ parser_library/src/workspaces/wildcard.cpp
@@ -24,24 +24,36 @@ const std::regex question("\\?");
 const std::regex nongreedy("(\\*|\\+)");
 const std::regex slash("\\\\");
 const std::regex file_scheme_windows("^file:///([A-Za-z])(?::|%3[aA])");
-const std::string single_url_char_matcher =
-    "(?:[^%//]|%[eE]2%82%[aA][cC]|%[eE]2%80%[9aAbB][0-9a-fA-F]|%[cCeE][2356bB]%["
-    "89aAbB][0-9a-fA-F]|%[0-9a-fA-F][0-9a-fA-F])";
+
+const std::string single_url_char_matcher = []() {
+    const std::string utf_8_continuation_matcher = "(?:%[89abAB][0-9a-fA-F])";
+    const std::string utf_8_1_byte_matcher = "%[0-7][0-9a-fA-F]";
+    const std::string utf_8_2_byte_matcher = "%[cCdD][0-9a-fA-F]" + utf_8_continuation_matcher;
+    const std::string utf_8_3_byte_matcher = "%[eE][0-9a-fA-F]" + utf_8_continuation_matcher + "{2}";
+    const std::string utf_8_4_byte_matcher = "%[fF][0-7]" + utf_8_continuation_matcher + "{3}";
+
+    const std::string utf_8_char_matcher =
+        utf_8_4_byte_matcher + "|" + utf_8_3_byte_matcher + "|" + utf_8_2_byte_matcher + "|" + utf_8_1_byte_matcher;
+
+    return "(?:[^%//]|" + utf_8_char_matcher + ")";
+}();
 
 std::pair<size_t, char> match_windows_uri_with_drive(const std::string& input)
 {
-    if (std::smatch matches; std::regex_search(input, matches, std::regex("^file:///([a-zA-Z])(?::|%3[aA])")))
+    if (std::smatch matches; std::regex_search(input, matches, file_scheme_windows))
         return { matches[0].length(), matches[1].str()[0] };
     else
         return { 0, '\0' };
 }
 
-size_t preprocess_uri_with_windows_drive_letter_regex_string(const std::string& input, std::string& r)
+std::pair<size_t, std::string> preprocess_uri_with_windows_drive_letter_regex_string(const std::string& input)
 {
     auto [match_length, drive_letter] = match_windows_uri_with_drive(input);
 
     if (match_length == 0)
-        return 0;
+        return { 0, "" };
+
+    std::string r;
 
     // Append windows file path (e.g. ^file:///[cC](?::|%3[aA]))
     r.append("^file:///[");
@@ -49,7 +61,7 @@ size_t preprocess_uri_with_windows_drive_letter_regex_string(const std::string& 
     r.push_back(static_cast<char>(toupper(drive_letter)));
     r.append("](?::|%3[aA])");
 
-    return match_length;
+    return { match_length, r };
 }
 } // namespace
 
@@ -61,15 +73,9 @@ std::regex wildcard2regex(std::string wildcard)
     wildcard = std::regex_replace(wildcard, question, single_url_char_matcher);
     wildcard = std::regex_replace(wildcard, nongreedy, ".$1?");
 
-    if (std::smatch this_smatch;
-        utils::platform::is_windows() && std::regex_search(wildcard, this_smatch, file_scheme_windows))
+    if (auto [match_length, regex] = preprocess_uri_with_windows_drive_letter_regex_string(wildcard); match_length != 0)
     {
-        std::string regex;
-        regex = "file:///(?:[";
-        regex.push_back(static_cast<char>(tolower(this_smatch[1].str()[0])));
-        regex.push_back(static_cast<char>(toupper(this_smatch[1].str()[0])));
-        regex.append("])(?::|%3[aA])");
-        regex.append(this_smatch.suffix());
+        regex.append(wildcard.substr(match_length));
 
         wildcard = std::move(regex);
     }
@@ -79,13 +85,13 @@ std::regex wildcard2regex(std::string wildcard)
 
 std::regex pathmask_to_regex(const std::string& input)
 {
-    std::string r;
+    // URI mask shouldn't care about Windows Drive letter case
+    auto [match_length, r] = preprocess_uri_with_windows_drive_letter_regex_string(input);
     r.reserve(input.size());
 
+    // Remove prefix of the preprocessed windows drive letter
     std::string_view s = input;
-
-    // URI mask shouldn't care about Windows Drive letter case
-    s.remove_prefix(preprocess_uri_with_windows_drive_letter_regex_string(input, r));
+    s.remove_prefix(match_length);
 
     bool path_started = false;
     while (!s.empty())
