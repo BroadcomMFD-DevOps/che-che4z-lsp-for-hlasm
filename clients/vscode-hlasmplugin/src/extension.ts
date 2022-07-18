824367e3f00ff62eaca7d574f57e227e6fc3ab9d
Slavomir Kucera <slavomir.kucera@broadcom.com>
2022-07-18 08:55:36 +0200

    review suggestions

--- clients/vscode-hlasmplugin/src/extension.ts
+++ clients/vscode-hlasmplugin/src/extension.ts
@@ -143,7 +143,7 @@ async function registerToContext(context: vscode.ExtensionContext, client: vscod
     var commandList = await vscode.commands.getCommands();
 
     // check whether the continuation commands have already been registered
-    var commandsRegistered = commandList.find(command => command == 'insertContinuation' || command == 'removeContinuation');
+    var commandsRegistered = commandList.find(command => command == "extension.hlasm-plugin.insertContinuation" || command == "extension.hlasm-plugin.removeContinuation");
 
     // initialize helpers
     const handler = new EventsHandler(completeCommand);
@@ -161,11 +161,11 @@ async function registerToContext(context: vscode.ExtensionContext, client: vscod
 
     // register continuation handlers
     if (!commandsRegistered) {
-        context.subscriptions.push(vscode.commands.registerTextEditorCommand("insertContinuation",
+        context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.hlasm-plugin.insertContinuation",
             (editor, edit) => contHandling.insertContinuation(editor, edit, offset, continueColumn)));
-        context.subscriptions.push(vscode.commands.registerTextEditorCommand("removeContinuation",
+        context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.hlasm-plugin.removeContinuation",
             (editor, edit) => contHandling.removeContinuation(editor, edit, offset)));
-        context.subscriptions.push(vscode.commands.registerTextEditorCommand("rearrangeSequenceNumbers",
+        context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.hlasm-plugin.rearrangeSequenceNumbers",
             (editor, edit) => contHandling.rearrangeSequenceNumbers(editor, edit, offset)));
     }
 
@@ -195,7 +195,7 @@ async function registerToContext(context: vscode.ExtensionContext, client: vscod
 
     context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider("hlasm", new HLASMVirtualFileContentProvider(client)));
 
-    context.subscriptions.push(vscode.commands.registerCommand("extension.hlasm-plugin.downloadDependencies", () => downloadDependencies(context)))
+    context.subscriptions.push(vscode.commands.registerCommand("extension.hlasm-plugin.downloadDependencies", () => downloadDependencies(context)));
 
     return handler;
 }
