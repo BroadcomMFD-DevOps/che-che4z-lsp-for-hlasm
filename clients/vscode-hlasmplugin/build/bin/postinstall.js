ebc3535c8321c7a2965691ba1510ec491cb80f31
Slavomir Kucera <slavomir.kucera@broadcom.com>
2022-07-18 09:14:32 +0200

    upgrade to basid-ftp 5

--- clients/vscode-hlasmplugin/build/bin/postinstall.js
+++ clients/vscode-hlasmplugin/build/bin/postinstall.js
@@ -18,12 +18,6 @@ const path = require('path');
 
 const base = path.join(__dirname, "..", "..");
 
-// patch basic-ftp@4.6.6 data socket close issue
-const transfer = path.join(base, "node_modules/basic-ftp/dist/transfer.js");
-const content = fs.readFileSync(transfer).toString();
-const new_content = content.replace(/source.pipe\(dataSocket\).once\("finish", \(\) => {.*?}\)/s, 'source.pipe(dataSocket.once("close", () => { resolver.onDataDone(task); }))');
-fs.writeFileSync(transfer, new_content);
-
 fs.mkdirSync(path.join(base, "bin"), { recursive: true });
 fs.copyFileSync(path.join(base, "terse", "terse.js"), path.join(base, "bin", "terse.js"));
 fs.copyFileSync(path.join(base, "terse", "terse.wasm"), path.join(base, "bin", "terse.wasm"));
