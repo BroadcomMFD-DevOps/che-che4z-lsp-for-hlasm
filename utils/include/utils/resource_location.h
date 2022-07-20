f02240310815c8fbe5bdfb45df45acd5b79ed773
Jiri Mosinger <jiri.mosinger@broadcom.com>
2022-07-20 11:04:47 +0200

    Removed custom spaceship operation implementation

--- utils/include/utils/resource_location.h
+++ utils/include/utils/resource_location.h
@@ -51,8 +51,7 @@ public:
     void relative_reference_resolution(const std::string& other);
     static resource_location relative_reference_resolution(resource_location rl, const std::string& other);
 
-    std::strong_ordering operator<=>(const resource_location& rl) const noexcept;
-    bool operator==(const resource_location& rl) const noexcept;
+    std::strong_ordering operator<=>(const resource_location& rl) const noexcept = default;
 
 private:
     std::string m_uri;
