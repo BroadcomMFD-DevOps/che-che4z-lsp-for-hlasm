98edbecff18e55955487459410be979dd224894c
Jiri Mosinger <jiri.mosinger@broadcom.com>
2022-09-15 08:35:44 +0200

    ...

--- parser_library/src/processing/instruction_sets/ca_processor.cpp
+++ parser_library/src/processing/instruction_sets/ca_processor.cpp
@@ -231,10 +231,10 @@ bool ca_processor::prepare_GBL_LCL(const semantics::complete_statement& stmt, st
                 continue;
             }
 
-            if (std::find_if(info.begin(), info.end(), [id](const auto& i) { return i.id == id; }) != info.end())
+            if (std::find_if(info.begin(), info.end(), [id = id](const auto& i) { return i.id == id; }) != info.end())
                 add_diagnostic(diagnostic_op::error_E051(*id, ca_op->operand_range));
             else
-                info.emplace_back(id, subscript.empty(), ca_op->operand_range);
+                info.push_back({ id, subscript.empty(), ca_op->operand_range });
         }
         else
         {
