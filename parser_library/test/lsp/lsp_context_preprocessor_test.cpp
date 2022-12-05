49d86d63c2997f5b565429625f860ec96f33851a
Jiri Mosinger <jiri.mosinger@broadcom.com>
2022-12-05 14:29:24 +0100

    Dynamic lookup of preproc files + todos

--- parser_library/test/lsp/lsp_context_preprocessor_test.cpp
+++ parser_library/test/lsp/lsp_context_preprocessor_test.cpp
@@ -13,6 +13,7 @@
  */
 
 #include <algorithm>
+#include <optional>
 
 #include "gtest/gtest.h"
 
@@ -21,7 +22,6 @@
 #include "context/instruction.h"
 #include "utils/resource_location.h"
 #include "workspaces/parse_lib_provider.h"
-
 
 using namespace hlasm_plugin::parser_library;
 using namespace hlasm_plugin::utils::resource;
@@ -32,7 +32,6 @@ const resource_location mac_loc("MAC");
 const resource_location source_loc("OPEN");
 const resource_location member_loc("MEMBER");
 const resource_location member2_loc("MEMBER2");
-const resource_location preproc6_loc("PREPROCESSOR_6.hlasm");
 } // namespace
 
 class lsp_context_endevor_preprocessor_test : public testing::Test
@@ -136,7 +135,27 @@ public:
     lsp_context_cics_preprocessor_test()
         : a(contents, analyzer_options { source_loc, cics_preprocessor_options() }) {};
 
-    void SetUp() override { a.analyze(); }
+    void SetUp() override
+    {
+        a.analyze();
+
+        preproc1_loc = find_preproc_file("PREPROCESSOR_1.hlasm");
+        preproc6_loc = find_preproc_file("PREPROCESSOR_6.hlasm");
+
+        ASSERT_TRUE(preproc1_loc.has_value());
+        ASSERT_TRUE(preproc6_loc.has_value());
+    }
+
+private:
+    std::optional<resource_location> find_preproc_file(std::string_view name)
+    {
+        const auto& files = a.hlasm_ctx().get_visited_files();
+
+        auto it = std::find_if(
+            files.begin(), files.end(), [name](const resource_location& f) { return f.get_uri().ends_with(name); });
+
+        return it != files.end() ? std::make_optional(*it) : std::nullopt;
+    }
 
 protected:
     const std::string contents =
@@ -148,15 +167,24 @@ A   EXEC CICS ABEND ABCODE('1234') NODUMP
 
 B   LARL 0,DFHRESP(NORMAL)
     L   0,DFHVALUE ( BUSY )
-    L     0,DFHRESP ( NORMAL ))";
+    L     0,DFHRESP ( NORMAL )
+
+    LARL 1,A
+    LARL 1,B)";
 
     analyzer a;
+    std::optional<resource_location> preproc1_loc;
+    std::optional<resource_location> preproc6_loc;
 };
 
 TEST_F(lsp_context_cics_preprocessor_test, go_to)
 {
-    // no jump, label A
-    EXPECT_EQ(location(position(1, 1), source_loc), a.context().lsp_ctx->definition(source_loc, position(1, 1)));
+    // Todo make the 2 step definition() work
+    //// Jump to label in main document, label A
+    // EXPECT_EQ(location(position(1, 0), source_loc), a.context().lsp_ctx->definition(source_loc, position(10, 12)));
+    //// Jump to label in virtual file, label A
+    // EXPECT_EQ(location(position(1, 0), *preproc1_loc), a.context().lsp_ctx->definition(source_loc, position(1, 0)));
+
     // no jump, EXEC CICS ABEND
     EXPECT_EQ(location(position(1, 16), source_loc), a.context().lsp_ctx->definition(source_loc, position(1, 16)));
     // no jump, operand ABCODE('1234')
@@ -164,8 +192,12 @@ TEST_F(lsp_context_cics_preprocessor_test, go_to)
     // no jump, operand NODUMP
     EXPECT_EQ(location(position(1, 41), source_loc), a.context().lsp_ctx->definition(source_loc, position(1, 41)));
 
-    // jump to virtual file, label B
-    EXPECT_EQ(location(position(1, 0), preproc6_loc), a.context().lsp_ctx->definition(source_loc, position(6, 1)));
+    // Todo make the 2 step definition() work
+    //// Jump to label in main document, label B
+    // EXPECT_EQ(location(position(6, 0), source_loc), a.context().lsp_ctx->definition(source_loc, position(11, 12)));
+    //  Jump to label in virtual file, label B
+    EXPECT_EQ(location(position(1, 0), *preproc6_loc), a.context().lsp_ctx->definition(source_loc, position(6, 1)));
+
     // no jump, instr LARL
     EXPECT_EQ(location(position(6, 7), source_loc), a.context().lsp_ctx->definition(source_loc, position(6, 7)));
     // no jump, operand 0
@@ -230,13 +262,15 @@ TEST_F(lsp_context_cics_preprocessor_test, refs_dfh)
 {
     const location_list expected_larl_locations {
         location(position(6, 4), source_loc),
-        location(position(1, 9), preproc6_loc),
+        location(position(1, 9), *preproc6_loc),
+        location(position(10, 4), source_loc),
+        location(position(11, 4), source_loc),
     };
     const location_list expected_l_locations {
         location(position(7, 4), source_loc),
         location(position(8, 4), source_loc),
-        location(position(3, 9), preproc6_loc),
-        location(position(5, 9), preproc6_loc),
+        location(position(3, 9), *preproc6_loc),
+        location(position(5, 9), *preproc6_loc),
     };
     const location_list expected_dfhresp_normal_locations {
         location(position(6, 11), source_loc),
