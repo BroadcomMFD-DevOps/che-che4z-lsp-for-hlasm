60ff51dd416a1630d2b0ecdf6c4733bb6a50c650
Slavomir Kucera <slavomir.kucera@broadcom.com>
2022-12-23 15:43:54 +0100

    ...

--- clients/vscode-hlasmplugin/src/test/suite/codeActions.test.ts
+++ clients/vscode-hlasmplugin/src/test/suite/codeActions.test.ts
@@ -16,9 +16,27 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as helper from './testHelper';
 
+async function queryCodeActions(uri: vscode.Uri, range: vscode.Range, sleep: number, max_attempts: number = 10) {
+    for (; max_attempts > 0; --max_attempts) {
+        // it seems that vscode occasionally makes its own request and cancels ours
+        await helper.sleep(sleep);
+
+        try {
+            const codeActionsList: vscode.CodeAction[] = await vscode.commands.executeCommand('vscode.executeCodeActionProvider', uri, range);
+
+            // empty list also points toward the request being cancelled
+            if (codeActionsList.length !== 0)
+                return codeActionsList;
+        } catch (e) {
+            assert.equal(e.message, 'Cancelled');
+        }
+    }
+    return [];
+}
+
 suite('Code actions', () => {
     suiteSetup(async function () {
-        this.timeout(10000);
+        this.timeout(20000);
     });
 
     suiteTeardown(async function () {
@@ -34,9 +52,11 @@ suite('Code actions', () => {
 
         await diagnostic_event;
 
-        const codeActionsList: vscode.CodeAction[] = await vscode.commands.executeCommand('vscode.executeCodeActionProvider', document.uri, new vscode.Range(0, 10, 0, 15));
+        const codeActionsList = await queryCodeActions(document.uri, new vscode.Range(0, 10, 0, 15), 500);
 
         assert.equal(codeActionsList.length, 4 + 3);
+
+        await helper.closeAllEditors();
     }).timeout(10000).slow(5000);
 
     test('Diagnostics suppressed', async () => {
@@ -47,8 +67,10 @@ suite('Code actions', () => {
 
         await diagnostic_event;
 
-        const codeActionsList: vscode.CodeAction[] = await vscode.commands.executeCommand('vscode.executeCodeActionProvider', document.uri, new vscode.Range(0, 10, 0, 15));
+        const codeActionsList = await queryCodeActions(document.uri, new vscode.Range(0, 10, 0, 15), 500);
 
         assert.equal(codeActionsList.length, 1);
+
+        await helper.closeAllEditors();
     }).timeout(10000).slow(5000);
 });
