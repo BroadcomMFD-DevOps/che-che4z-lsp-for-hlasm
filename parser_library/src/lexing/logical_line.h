e138a3597551e3d66dab3834fc32fd39a54eb687
Jiri Mosinger <jiri.mosinger@broadcom.com>
2023-01-05 11:09:19 +0100

    Include

--- parser_library/src/lexing/logical_line.h
+++ parser_library/src/lexing/logical_line.h
@@ -15,7 +15,6 @@
 #ifndef HLASMPLUGIN_HLASMPARSERLIBRARY_LOGICAL_LINE_H
 #define HLASMPLUGIN_HLASMPARSERLIBRARY_LOGICAL_LINE_H
 
-#include <algorithm>
 #include <cassert>
 #include <cstddef>
 #include <cstdlib>
