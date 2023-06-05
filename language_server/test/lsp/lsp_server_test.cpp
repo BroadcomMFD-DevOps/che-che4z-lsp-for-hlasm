571b0f3f7135dee7759d71c3540f75d8d6cf34cf
Slavomir Kucera <slavomir.kucera@broadcom.com>
2023-06-05 15:00:34 +0200

    lsp server test

--- language_server/test/lsp/lsp_server_test.cpp
+++ language_server/test/lsp/lsp_server_test.cpp
@@ -30,6 +30,7 @@ namespace nlohmann {
 inline void PrintTo(nlohmann::json const& json, std::ostream* os) { *os << json.dump(); }
 } // namespace nlohmann
 
+using namespace ::testing;
 using namespace hlasm_plugin;
 using namespace language_server;
 
@@ -38,7 +39,7 @@ TEST(lsp_server, initialize)
     // this is json params actually sent by vscode LSP client
     auto j =
         R"({"jsonrpc":"2.0","id":47,"method":"initialize","params":{"processId":5236,"rootPath":null,"rootUri":null,"capabilities":{"workspace":{"applyEdit":true,"workspaceEdit":{"documentChanges":true},"didChangeConfiguration":{"dynamicRegistration":true},"didChangeWatchedFiles":{"dynamicRegistration":true},"symbol":{"dynamicRegistration":true,"symbolKind":{"valueSet":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]}},"executeCommand":{"dynamicRegistration":true},"configuration":true,"workspaceFolders":true},"textDocument":{"publishDiagnostics":{"relatedInformation":true},"synchronization":{"dynamicRegistration":true,"willSave":true,"willSaveWaitUntil":true,"didSave":true},"completion":{"dynamicRegistration":true,"contextSupport":true,"completionItem":{"snippetSupport":true,"commitCharactersSupport":true,"documentationFormat":["markdown","plaintext"],"deprecatedSupport":true,"preselectSupport":true},"completionItemKind":{"valueSet":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]}},"hover":{"dynamicRegistration":true,"contentFormat":["markdown","plaintext"]},"signatureHelp":{"dynamicRegistration":true,"signatureInformation":{"documentationFormat":["markdown","plaintext"]}},"definition":{"dynamicRegistration":true},"references":{"dynamicRegistration":true},"documentHighlight":{"dynamicRegistration":true},"documentSymbol":{"dynamicRegistration":true,"symbolKind":{"valueSet":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]}},"codeAction":{"dynamicRegistration":true,"codeActionLiteralSupport":{"codeActionKind":{"valueSet":["","quickfix","refactor","refactor.extract","refactor.inline","refactor.rewrite","source","source.organizeImports"]}}},"codeLens":{"dynamicRegistration":true},"formatting":{"dynamicRegistration":true},"rangeFormatting":{"dynamicRegistration":true},"onTypeFormatting":{"dynamicRegistration":true},"rename":{"dynamicRegistration":true},"documentLink":{"dynamicRegistration":true},"typeDefinition":{"dynamicRegistration":true},"implementation":{"dynamicRegistration":true},"colorProvider":{"dynamicRegistration":true}}},"trace":"off","workspaceFolders":null}})"_json;
-    test::ws_mngr_mock ws_mngr;
+    NiceMock<test::ws_mngr_mock> ws_mngr;
     send_message_provider_mock smpm;
     lsp::server s(ws_mngr);
     s.set_send_message_provider(&smpm);
@@ -92,7 +93,7 @@ TEST(lsp_server, initialize)
 TEST(lsp_server, not_implemented_method)
 {
     auto j = R"({"jsonrpc":"2.0","id":47,"method":"unknown_method","params":"A parameter"})"_json;
-    test::ws_mngr_mock ws_mngr;
+    NiceMock<test::ws_mngr_mock> ws_mngr;
     send_message_provider_mock smpm;
     lsp::server s(ws_mngr);
     s.set_send_message_provider(&smpm);
@@ -257,7 +258,7 @@ TEST(lsp_server, request_error_no_message)
 
 TEST(lsp_server_test, non_compliant_uri)
 {
-    test::ws_mngr_mock ws_mngr;
+    NiceMock<test::ws_mngr_mock> ws_mngr;
     ::testing::NiceMock<send_message_provider_mock> smpm;
     lsp::server s(ws_mngr);
     s.set_send_message_provider(&smpm);
@@ -267,4 +268,87 @@ TEST(lsp_server_test, non_compliant_uri)
 
     s.message_received(
         R"({"jsonrpc":"2.0","method":"textDocument/didOpen","params":{"textDocument":{"uri":"user_storage:/user/storage/layout","languageId":"plaintext","version":4,"text":"sad"}}})"_json);
+}
+
+TEST(lsp_server_test, external_configuration_invalidation)
+{
+    NiceMock<test::ws_mngr_mock> ws_mngr;
+    ::testing::NiceMock<send_message_provider_mock> smpm;
+    lsp::server s(ws_mngr);
+    s.set_send_message_provider(&smpm);
+
+    EXPECT_CALL(ws_mngr, invalidate_external_configuration(::testing::StrEq("scheme:path")));
+
+    s.message_received(
+        R"({"jsonrpc":"2.0","method":"invalidate_external_configuration","params":{"uri":"scheme:path"}})"_json);
+}
+
+TEST(lsp_server_test, external_configuration_request)
+{
+    using namespace ::testing;
+    parser_library::workspace_manager_requests* wmr = nullptr;
+    NiceMock<test::ws_mngr_mock> ws_mngr;
+    EXPECT_CALL(ws_mngr, set_request_interface(_)).WillOnce(SaveArg<0>(&wmr));
+
+    NiceMock<send_message_provider_mock> smpm;
+    lsp::server s(ws_mngr);
+
+    ASSERT_TRUE(wmr);
+
+    s.set_send_message_provider(&smpm);
+
+    struct resp_t
+    {
+        std::variant<std::string, std::pair<int, std::string>> result;
+
+        void provide(parser_library::sequence<char> c) { result = std::string(c); }
+        void error(int err, const char* errmsg) noexcept { result = std::pair<int, std::string>(err, errmsg); }
+    };
+
+    auto [c, i] = parser_library::make_workspace_manager_response(std::in_place_type<resp_t>);
+
+    EXPECT_CALL(smpm,
+        reply(
+            R"({"jsonrpc":"2.0","id":0,"method":"external_configuration_request","params":{"uri":"scheme:path"}})"_json));
+
+    wmr->request_file_configuration(parser_library::sequence<char>(std::string_view("scheme:path")), c);
+
+    s.message_received(R"({"jsonrpc":"2.0","id":0,"result":{"configuration":"GRP1"}})"_json);
+
+    EXPECT_EQ(i->result, (std::variant<std::string, std::pair<int, std::string>>(R"("GRP1")")));
+}
+
+TEST(lsp_server_test, external_configuration_request_error)
+{
+    parser_library::workspace_manager_requests* wmr = nullptr;
+    NiceMock<test::ws_mngr_mock> ws_mngr;
+    EXPECT_CALL(ws_mngr, set_request_interface(_)).WillOnce(SaveArg<0>(&wmr));
+
+    NiceMock<send_message_provider_mock> smpm;
+    lsp::server s(ws_mngr);
+
+    ASSERT_TRUE(wmr);
+
+    s.set_send_message_provider(&smpm);
+
+    struct resp_t
+    {
+        std::variant<std::string, std::pair<int, std::string>> result;
+
+        void provide(parser_library::sequence<char> c) { result = std::string(c); }
+        void error(int err, const char* errmsg) noexcept { result = std::pair<int, std::string>(err, errmsg); }
+    };
+
+    auto [c, i] = parser_library::make_workspace_manager_response(std::in_place_type<resp_t>);
+
+    EXPECT_CALL(smpm,
+        reply(
+            R"({"jsonrpc":"2.0","id":0,"method":"external_configuration_request","params":{"uri":"scheme:path"}})"_json));
+
+    wmr->request_file_configuration(parser_library::sequence<char>(std::string_view("scheme:path")), c);
+
+    s.message_received(R"({"jsonrpc":"2.0","id":0,"error":{"code":123456, "message":"error message"}})"_json);
+
+    EXPECT_EQ(i->result,
+        (std::variant<std::string, std::pair<int, std::string>>(std::pair<int, std::string>(123456, "error message"))));
 }
