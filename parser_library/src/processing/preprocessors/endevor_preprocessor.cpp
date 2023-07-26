74d51197647d8ac3f16d7a161b37d13c300ccee1
Slavomir Kucera <slavomir.kucera@broadcom.com>
2023-07-26 16:29:44 +0200

    fix: CICS status codes substitutions are not always performed

--- parser_library/src/processing/preprocessors/endevor_preprocessor.cpp
+++ parser_library/src/processing/preprocessors/endevor_preprocessor.cpp
@@ -161,7 +161,8 @@ public:
             {
                 static const stmt_part_ids part_ids { std::nullopt, { 1 }, 2, 3 };
 
-                auto stmt = get_preproc_statement<semantics::endevor_statement_si>(matches, part_ids, *line_no, true);
+                auto stmt = get_preproc_statement<semantics::endevor_statement_si>(
+                    std::span(make_preproc_matches<3>(matches)), part_ids, *line_no, true);
                 do_highlighting(*stmt, m_src_proc);
                 set_statement(std::move(stmt));
             }
