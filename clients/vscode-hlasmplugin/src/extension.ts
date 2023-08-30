fc93ebede6f86044ae38655f9b4fe62011e96439
Slavomir Kucera <slavomir.kucera@broadcom.com>
2023-08-30 10:04:09 +0200

    external files interface upgrades

--- clients/vscode-hlasmplugin/src/extension.ts
+++ clients/vscode-hlasmplugin/src/extension.ts
@@ -31,7 +31,7 @@ import { HLASMCodeActionsProvider } from './hlasmCodeActionsProvider';
 import { hlasmplugin_folder, hlasmplugin_folder_filter, bridge_json_filter } from './constants';
 import { ConfigurationsHandler } from './configurationsHandler';
 import { getLanguageClientMiddleware } from './languageClientMiddleware';
-import { ClientInterface, ClientUriDetails, HLASMExternalFiles } from './hlasmExternalFiles';
+import { HLASMExternalFiles } from './hlasmExternalFiles';
 import { HLASMExternalFilesFtp } from './hlasmExternalFilesFtp';
 import { HLASMExternalConfigurationProvider, HLASMExternalConfigurationProviderHandler } from './hlasmExternalConfigurationProvider';
 import { HlasmExtension } from './extension.interface';
@@ -143,14 +143,15 @@ export async function activate(context: vscode.ExtensionContext): Promise<HlasmE
     // register all commands and objects to context
     await registerToContext(context, hlasmpluginClient, telemetry, extFiles);
 
-    let api = {
-        registerExternalFileClient<ConnectArgs, ReadArgs extends ClientUriDetails, ListArgs extends ClientUriDetails>(service: string, client: ClientInterface<ConnectArgs, ReadArgs, ListArgs>) {
+    let api: HlasmExtension = {
+        registerExternalFileClient(service, client) {
             extFiles.setClient(service, client);
         },
         registerExternalConfigurationProvider(h: HLASMExternalConfigurationProviderHandler) {
             return extConfProvider.addHandler(h);
         },
     };
+
     return api;
 }
 
