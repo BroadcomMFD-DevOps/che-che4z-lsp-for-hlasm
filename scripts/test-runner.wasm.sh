4eca150da42a6fbd2557130b000d7af76ce2c7b5
Slavomir Kucera <slavomir.kucera@broadcom.com>
2023-09-11 09:29:58 +0200

    exit on failure

--- scripts/test-runner.wasm.sh
+++ scripts/test-runner.wasm.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-node "$@"
+set -e
+node --experimental-wasm-threads --experimental-wasm-bulk-memory $1.js
+node --experimental-wasm-threads --experimental-wasm-bulk-memory --input-type=module --eval "import('$1.mjs').then(m=>{m.default({web:true})})"
