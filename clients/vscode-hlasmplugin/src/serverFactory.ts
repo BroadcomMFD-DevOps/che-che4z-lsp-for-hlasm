c9f501821c4162307a7c0e924d1b0704c97e0c06
Slavomir Kucera <slavomir.kucera@broadcom.com>
2024-03-06 10:29:48 +0100

    linux arm

--- clients/vscode-hlasmplugin/src/serverFactory.ts
+++ clients/vscode-hlasmplugin/src/serverFactory.ts
@@ -23,7 +23,8 @@ import { decorateArgs, ServerVariant } from './serverFactory.common';
 const supportedNativePlatforms: Readonly<{ [key: string]: string }> = Object.freeze({
     'win32.x64': 'win32_x64',
     'win32.arm64': 'win32_arm64',
-    'linux.x64': 'linux',
+    'linux.x64': 'linux_x64',
+    'linux.arm64': 'linux_arm64',
     'darwin.x64': 'darwin_x64',
     'darwin.arm64': 'darwin_arm64',
 });
---
+++ scripts/test-runner.linux_arm64.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -e
+qemu-aarch64 $1
