e7871287d7b13d72bc7cdaf28604e93e5525f8f3
Slavomir Kucera <slavomir.kucera@broadcom.com>
2024-03-06 09:48:18 +0100

    windows arm

--- clients/vscode-hlasmplugin/src/serverFactory.ts
+++ clients/vscode-hlasmplugin/src/serverFactory.ts
@@ -21,7 +21,8 @@ import { getConfig } from './eventsHandler';
 import { decorateArgs, ServerVariant } from './serverFactory.common';
 
 const supportedNativePlatforms: Readonly<{ [key: string]: string }> = Object.freeze({
-    'win32.x64': 'win32',
+    'win32.x64': 'win32_x64',
+    'win32.arm64': 'win32_arm64',
     'linux.x64': 'linux',
     'darwin.x64': 'darwin_x64',
     'darwin.arm64': 'darwin_arm64',
