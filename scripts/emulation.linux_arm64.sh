bdb35135989ea93f32c00c6195a9d87ddc5ed0de
Slavomir Kucera <slavomir.kucera@broadcom.com>
2024-03-07 13:24:19 +0100

    go to the right directory

--- scripts/emulation.linux_arm64.sh
+++ scripts/emulation.linux_arm64.sh
@@ -4,9 +4,11 @@ set -e
 
 sudo apt-get update
 sudo apt-get install -y qemu-user
+
 mkdir -p bin/linux_x64
+cd bin/linux_x64
 
 echo '#!/usr/bin/env sh' > language_server
-echo 'qemu-aarch64 $(dirname "$0")/../linux_arm64/language_server "$@"' >> language_server
+echo 'qemu-aarch64 "$(dirname "$0")/../linux_arm64/language_server" "$@"' >> language_server
 
 chmod +x language_server
