84db292b7f45c8349428d968f6ef5b310c6a5f8f
Slavomir Kucera <slavomir.kucera@broadcom.com>
2024-04-05 14:25:23 +0200

    ... [skip ci]

--- clients/neovim/lspconfig.hlasm.lua
+++ clients/neovim/lspconfig.hlasm.lua
@@ -9,7 +9,7 @@ return {
   },
   docs = {
     description = [[
-    `hlasm-language-server` is a language server for the High Level Assembler language used on IBM SystemZ mainframes.
+    `hlasm_language_server` is a language server for the High Level Assembler language used on IBM SystemZ mainframes.
 
     To learn how to configure the HLASM language server, see the [HLASM Language Support documentation](https://github.com/eclipse-che4z/che-che4z-lsp-for-hlasm).
     ]],
