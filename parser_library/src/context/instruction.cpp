770f1debe813da7c58098388453bb39c3d3ac549
Slavomir Kucera <slavomir.kucera@broadcom.com>
2024-04-30 09:39:19 +0200

    refactor mach op checking

--- parser_library/src/context/instruction.cpp
+++ parser_library/src/context/instruction.cpp
@@ -429,17 +429,16 @@ bool hlasm_plugin::parser_library::context::machine_instruction::check(std::stri
         return false;
     }
     bool error = false;
-    for (size_t i = 0; i < to_check.size(); i++)
+    for (const auto* fmt = m_operands; const auto* op : to_check)
     {
-        assert(to_check[i] != nullptr);
-        diagnostic_op diag;
-        if (!(*to_check[i]).check(diag, m_operands[i], name_of_instruction, stmt_range))
+        assert(op != nullptr);
+        if (auto diag = op->check(*fmt++, name_of_instruction, stmt_range); diag.has_value())
         {
-            add_diagnostic(diag);
+            add_diagnostic(std::move(diag).value());
             error = true;
         }
     };
-    return (!error);
+    return !error;
 }
 
 template<mach_format F, const machine_operand_format&... Ops>
