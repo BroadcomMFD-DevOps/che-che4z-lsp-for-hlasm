4f0bc88f98260829f5e685bc9f958d647f921ba0
Slavomir Kucera <slavomir.kucera@broadcom.com>
2024-10-02 12:53:30 +0200

    rename

--- clients/vscode-hlasmplugin/src/test/suite/hlasmExternalFilesMF.test.ts
+++ clients/vscode-hlasmplugin/src/test/suite/hlasmExternalFilesMF.test.ts
@@ -23,15 +23,15 @@ const extensionContextMock = undefined as any as ExtensionContext;
 suite('External files (MF)', () => {
 
     test('Dataset parsing', async () => {
-        const ftpClient = HLASMExternalFilesMF(extensionContextMock);
+        const client = HLASMExternalFilesMF(extensionContextMock);
 
-        assert.strictEqual(await ftpClient.parseArgs('aaa', ExternalRequestType.list_directory), null);
-        assert.strictEqual(await ftpClient.parseArgs('/0', ExternalRequestType.list_directory), null);
-        assert.strictEqual(await ftpClient.parseArgs('/AAAAAAAAA', ExternalRequestType.list_directory), null);
-        assert.strictEqual(await ftpClient.parseArgs('/AAAAAAAA/BBBBBBB', ExternalRequestType.list_directory), null);
-        assert.strictEqual(await ftpClient.parseArgs('/AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAA.A', ExternalRequestType.list_directory), null);
+        assert.strictEqual(await client.parseArgs('aaa', ExternalRequestType.list_directory), null);
+        assert.strictEqual(await client.parseArgs('/0', ExternalRequestType.list_directory), null);
+        assert.strictEqual(await client.parseArgs('/AAAAAAAAA', ExternalRequestType.list_directory), null);
+        assert.strictEqual(await client.parseArgs('/AAAAAAAA/BBBBBBB', ExternalRequestType.list_directory), null);
+        assert.strictEqual(await client.parseArgs('/AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAA.A', ExternalRequestType.list_directory), null);
 
-        const full_length = await ftpClient.parseArgs('/aaaaaaaa.aaaaaaaa.aaaaaaaa.aaaaaaaa.aaaaaaaa', ExternalRequestType.list_directory);
+        const full_length = await client.parseArgs('/aaaaaaaa.aaaaaaaa.aaaaaaaa.aaaaaaaa.aaaaaaaa', ExternalRequestType.list_directory);
         assert.ok(full_length);
         assert.strictEqual(full_length.details.toDisplayString(), 'AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA');
         assert.strictEqual(full_length.details.normalizedPath(), '/AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA/');
@@ -39,19 +39,19 @@ suite('External files (MF)', () => {
     });
 
     test('Dataset member parsing', async () => {
-        const ftpClient = HLASMExternalFilesMF(extensionContextMock);
+        const client = HLASMExternalFilesMF(extensionContextMock);
 
-        assert.strictEqual(await ftpClient.parseArgs('aaa', ExternalRequestType.read_file), null);
-        assert.strictEqual(await ftpClient.parseArgs('/0', ExternalRequestType.read_file), null);
-        assert.strictEqual(await ftpClient.parseArgs('/AAAAAAAAA', ExternalRequestType.read_file), null);
-        assert.strictEqual(await ftpClient.parseArgs('/AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAA.A', ExternalRequestType.read_file), null);
+        assert.strictEqual(await client.parseArgs('aaa', ExternalRequestType.read_file), null);
+        assert.strictEqual(await client.parseArgs('/0', ExternalRequestType.read_file), null);
+        assert.strictEqual(await client.parseArgs('/AAAAAAAAA', ExternalRequestType.read_file), null);
+        assert.strictEqual(await client.parseArgs('/AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAA.A', ExternalRequestType.read_file), null);
 
-        assert.strictEqual(await ftpClient.parseArgs('/AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA', ExternalRequestType.read_file), null);
-        assert.strictEqual(await ftpClient.parseArgs('/AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA/0', ExternalRequestType.read_file), null);
+        assert.strictEqual(await client.parseArgs('/AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA', ExternalRequestType.read_file), null);
+        assert.strictEqual(await client.parseArgs('/AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA/0', ExternalRequestType.read_file), null);
 
-        assert.strictEqual(await ftpClient.parseArgs('/AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA/BBBBBBBBB', ExternalRequestType.read_file), null);
+        assert.strictEqual(await client.parseArgs('/AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA/BBBBBBBBB', ExternalRequestType.read_file), null);
 
-        const full_length = await ftpClient.parseArgs('/aaaaaaaa.aaaaaaaa.aaaaaaaa.aaaaaaaa.aaaaaaaa/bbbbbbbb', ExternalRequestType.read_file);
+        const full_length = await client.parseArgs('/aaaaaaaa.aaaaaaaa.aaaaaaaa.aaaaaaaa.aaaaaaaa/bbbbbbbb', ExternalRequestType.read_file);
         assert.ok(full_length);
         assert.strictEqual(full_length.details.toDisplayString(), 'AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA(BBBBBBBB)');
         assert.strictEqual(full_length.details.normalizedPath(), '/AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA.AAAAAAAA/BBBBBBBB');
