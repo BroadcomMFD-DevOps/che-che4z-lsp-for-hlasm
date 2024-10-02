b2ccf07a698752012e4d988ba8ac48680710ab8e
Slavomir Kucera <slavomir.kucera@broadcom.com>
2024-10-02 16:54:45 +0200

    adjust dataset client

--- clients/vscode-hlasmplugin/src/hlasmExternalFilesMF.ts
+++ clients/vscode-hlasmplugin/src/hlasmExternalFilesMF.ts
@@ -15,10 +15,13 @@
 import * as vscode from 'vscode';
 import * as ftp from 'basic-ftp';
 import { ClientInterface, ClientUriDetails, ExternalRequestType, SuspendError } from './hlasmExternalFiles';
-import { FtpConnectionInfo, gatherConnectionInfo, getLastRunConfig, translateConnectionInfo, updateLastRunConfig } from './ftpCreds';
+import { FtpConnectionInfo, ZoweConnectionInfo, gatherConnectionInfo, getLastRunConfig, translateConnectionInfo, updateLastRunConfig } from './ftpCreds';
 import { FBWritable } from './FBWritable';
 import { ConnectionPool } from './connectionPool';
 import { AsyncMutex } from './asyncMutex';
+import { Writable } from 'stream';
+import { concat } from './helpers';
+import { textDecode } from './tools.common';
 
 const checkResponse = (resp: ftp.FTPResponse) => {
     if (resp.code < 200 || resp.code > 299) throw Error("FTP Error: " + resp.message);
@@ -49,34 +52,156 @@ class DatasetUriDetails implements ClientUriDetails {
 const connectionPoolSize = 4;
 const connectionPoolTimeout = 30000;
 
+type MFClient = {
+    list(dataset: string): Promise<string[] | null>;
+    read(dataset: string, member: string): Promise<string | null>;
+    close(): void;
+    closed: boolean;
+}
+
+function isZoweCredentialError(e: Error): boolean {
+    if (!('mDetails' in e)) return false;
+    if (!(e.mDetails instanceof Object)) return false;
+    if (!('errorCode' in e.mDetails)) return false;
+    return e.mDetails.errorCode === 401;
+}
+
+async function ZoweAsMFClient(info: ZoweConnectionInfo): Promise<MFClient> {
+    const mvs = info.zoweExplorerApi.getMvsApi(info.loadedProfile);
+    return {
+        list: async (dataset: string): Promise<string[] | null> => {
+            try {
+                const { apiResponse: resp } = await mvs.allMembers(dataset);
+                return resp.items.map((x: { member: string }) => x.member);
+            } catch (e) {
+                if (e instanceof Error && isZoweCredentialError(e)) throw new SuspendError(e);
+                return null;
+            }
+        },
+        read: async (dataset: string, member: string): Promise<string | null> => {
+            try {
+                class StringWritable extends Writable {
+                    private chunks: Uint8Array[] = [];
+                    _write(chunk: Buffer, encoding: BufferEncoding, callback: (error?: Error | null) => void) {
+                        this.chunks.push(chunk);
+                        callback();
+                    }
+                    getResult() { return textDecode(concat(...this.chunks)); }
+                }
+                const stream = new StringWritable();
+                const content = await mvs.getContents(`${dataset}(${member})`, { stream });
+                if (content.success)
+                    return stream.getResult();
+                else
+                    return null;
+            } catch (e) {
+                if (e instanceof Error && isZoweCredentialError(e)) throw new SuspendError(e);
+                return null;
+            }
+
+        },
+        close: () => { },
+        get closed() { return false; },
+    }
+}
+
+function ftpProfileToServerId(info: FtpConnectionInfo): string {
+    return info.host + ':' + (info.port ?? '21');
+}
+
+const defaultPorts = Object.freeze({
+    http: 80,
+    https: 443,
+});
+
+function knownProtocol(p: string): p is keyof typeof defaultPorts {
+    return p in defaultPorts;
+}
+
+function zoweProfileToServerId(info: ZoweConnectionInfo): string | undefined {
+    if (!('profile' in info.loadedProfile)) return undefined;
+    const profile = info.loadedProfile.profile;
+    if (typeof profile !== 'object' || !profile) return undefined;
+    const host = 'host' in profile ? profile.host : undefined;
+    const port = 'port' in profile ? profile.port : undefined;
+    const protocol = 'protocol' in profile ? profile.protocol : undefined;
+    if (!host || typeof host !== 'string') return undefined;
+    if (typeof port === 'number')
+        return host + ':' + port;
+    if (port !== undefined)
+        return undefined;
+    if (typeof protocol !== 'string')
+        return undefined;
+    if (knownProtocol(protocol))
+        return host + ':' + defaultPorts[protocol];
+
+    return undefined;
+}
+
+async function FTPAsMFClient(info: FtpConnectionInfo): Promise<MFClient> {
+    const client = new ftp.Client();
+    client.parseList = (rawList: string): ftp.FileInfo[] => {
+        return rawList.split(/\r?\n/).slice(1).filter(x => !/^\s*$/.test(x)).map(value => new ftp.FileInfo(value.split(/\s/, 1)[0]));
+    };
+    await client.access(translateConnectionInfo(info)).catch(e => { throw e instanceof ftp.FTPError && (e.code === 430 || e.code == 530) ? new SuspendError(e) : e; });
+
+    return {
+        list: async (dataset: string): Promise<string[] | null> => {
+            try {
+                await checkedCommand(client, 'TYPE A');
+                const members = await client.list(`'${dataset}(*)'`);
+                return members.map(x => x.name);
+            }
+            catch (e) {
+                if (e instanceof ftp.FTPError && e.code == 550)
+                    return null;
+                throw e;
+            }
+        },
+        read: async (dataset: string, member: string): Promise<string | null> => {
+            try {
+                const buffer = new FBWritable();
+                buffer.on('error', err => { throw err });
+
+                await checkedCommand(client, 'TYPE I');
+                checkResponse(await client.downloadTo(buffer, `'${dataset}(${member!})'`));
+
+                return buffer.getResult();
+            }
+            catch (e) {
+                if (e instanceof ftp.FTPError && e.code == 550)
+                    return null;
+                throw e;
+            }
+        },
+        close: () => {
+            client.close();
+        },
+        get closed() { return client.closed; },
+    }
+}
+
 export function HLASMExternalFilesMF(context: vscode.ExtensionContext): ClientInterface<undefined, DatasetUriDetails, DatasetUriDetails> {
-    let activeConnectionInfo: FtpConnectionInfo | undefined = undefined;
+    let activeConnectionInfo: FtpConnectionInfo | ZoweConnectionInfo | undefined = undefined;
 
     const mutex = new AsyncMutex();
     const getConnInfo = async () => {
         const last = getLastRunConfig(context);
         const info = await gatherConnectionInfo(last);
-        if ('zoweExplorerApi' in info)
-            throw Error("not supported");
-        await updateLastRunConfig(context, { host: info.host, user: info.user, jobcard: last.jobcard });
+        await updateLastRunConfig(context, { host: 'zoweExplorerApi' in info ? info.hostInput : info.host, user: info.user, jobcard: last.jobcard });
 
         return info;
     };
     let allowCredQuery = 0;
     let mirrorCredQuery = allowCredQuery;
 
-    const pool = new ConnectionPool<ftp.Client>({
+    const pool = new ConnectionPool<MFClient>({
         create: async () => {
-            const client = new ftp.Client();
-            client.parseList = (rawList: string): ftp.FileInfo[] => {
-                return rawList.split(/\r?\n/).slice(1).filter(x => !/^\s*$/.test(x)).map(value => new ftp.FileInfo(value.split(/\s/, 1)[0]));
-            };
             return mutex.locked(async () => {
                 const info = activeConnectionInfo ?? (pool.closeClients(), await getConnInfo());
                 activeConnectionInfo = undefined;
-                await client.access(translateConnectionInfo(info)).catch(e => { throw e instanceof ftp.FTPError && (e.code === 430 || e.code == 530) ? new SuspendError(e) : e; });
+                const client: MFClient = 'loadedProfile' in info ? await ZoweAsMFClient(info) : await FTPAsMFClient(info);
                 activeConnectionInfo = info;
-
                 return client;
             });
         },
@@ -102,37 +227,17 @@ export function HLASMExternalFilesMF(context: vscode.ExtensionContext): ClientIn
         },
 
         listMembers: async (args: DatasetUriDetails): Promise<string[] | null> => pool.withClient(async (client) => {
-            try {
-                await checkedCommand(client, 'TYPE A');
-                const list = await client.list(`'${args.dataset}(*)'`);
-                return list.map(x => `/${args.dataset}/${x.name}.hlasm`);
-            }
-            catch (e) {
-                if (e instanceof ftp.FTPError && e.code == 550)
-                    return null;
-                throw e;
-            }
+            const list = await client.list(args.dataset);
+            if (!list) return null;
+            return list.map(x => `/${args.dataset}/${x}.hlasm`);
         }),
 
         readMember: async (args: DatasetUriDetails): Promise<string | null> => pool.withClient(async (client) => {
-            try {
-                const buffer = new FBWritable();
-                buffer.on('error', err => { throw err });
-
-                await checkedCommand(client, 'TYPE I');
-                checkResponse(await client.downloadTo(buffer, `'${args.dataset}(${args.member!})'`));
-
-                return buffer.getResult();
-            }
-            catch (e) {
-                if (e instanceof ftp.FTPError && e.code == 550)
-                    return null;
-                throw e;
-            }
+            return client.read(args.dataset, args.member!);
         }),
 
         serverId: () => mutex.locked(async () => {
-            const transform = (arg: FtpConnectionInfo) => arg.host + ':' + (arg.port ?? '21');
+            const transform = (arg: FtpConnectionInfo | ZoweConnectionInfo) => 'loadedProfile' in arg ? zoweProfileToServerId(arg) : ftpProfileToServerId(arg);
 
             if (activeConnectionInfo)
                 return transform(activeConnectionInfo);
