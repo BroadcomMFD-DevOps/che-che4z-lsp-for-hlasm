315962244489f81b8160f4a2e234a2c95d8e9e21
Slavomir Kucera <slavomir.kucera@broadcom.com>
2025-03-26 15:05:13 +0100

    more tests

--- parser_library/test/context/macro_processing_stack_test.cpp
+++ parser_library/test/context/macro_processing_stack_test.cpp
@@ -191,7 +191,7 @@ TEST(macro_processing_stack, inline_nested_macro_with_copy)
     COPY COPYBOOK
     MEND
     MEND
-
+*
     MAC
     NESTED
 )";
@@ -200,4 +200,25 @@ TEST(macro_processing_stack, inline_nested_macro_with_copy)
 
     ASSERT_TRUE(matches_message_codes(a.diags(), { "MNOTE" }));
     EXPECT_TRUE(matches_diagnostic_stack(a.diags().front(), { "COPYBOOK", "opencode", "opencode" }));
+}
+
+TEST(macro_processing_stack, copy_whole_macro)
+{
+    mock_parse_lib_provider lib({
+        { "COPYBOOK", R"(
+    MACRO
+    MAC
+    MNOTE 'Hello'
+    MEND
+)" },
+    });
+    std::string input = R"(
+    COPY COPYBOOK
+    MAC
+)";
+    analyzer a(input, analyzer_options { opencode, &lib });
+    a.analyze();
+
+    ASSERT_TRUE(matches_message_codes(a.diags(), { "MNOTE" }));
+    EXPECT_TRUE(matches_diagnostic_stack(a.diags().front(), { "COPYBOOK", "opencode" }));
 }
