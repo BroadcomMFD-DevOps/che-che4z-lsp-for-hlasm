55a13989eccfc64376a0c534bf0fc0b09fd58bc5
Slavomir Kucera <slavomir.kucera@broadcom.com>
2025-06-27 14:13:11 +0200

    ...

--- language_server/src/lsp/watchers.cpp
+++ language_server/src/lsp/watchers.cpp
@@ -19,6 +19,26 @@
 
 namespace hlasm_plugin::language_server::lsp {
 
+enum class watch_kind
+{
+    _default = 0,
+    _create = 1,
+    _change = 2,
+    _delete = 4,
+};
+
+[[nodiscard]] constexpr watch_kind operator|(watch_kind l, watch_kind r) noexcept
+{
+    return static_cast<watch_kind>(static_cast<int>(l) | static_cast<int>(r));
+}
+
+struct watcher
+{
+    std::string_view pattern;
+    std::string_view base = {};
+    watch_kind kind = watch_kind::_default;
+};
+
 void to_json(nlohmann::json& j, const watcher& w)
 {
     if (w.base.empty())
@@ -87,10 +107,10 @@ nlohmann::json default_watcher_registration()
             {
                 {
                     "watchers",
-                    nlohmann::json::array_t {
-                        { { "globPattern", "**/*" } },
-                        { { "globPattern", ".hlasmplugin/*.json" } },
-                    },
+                    std::to_array({
+                        watcher { .pattern = "**/*" },
+                        watcher { .pattern = ".hlasmplugin/*.json" },
+                    }),
                 },
             },
         },
--- language_server/src/lsp/watchers.h
+++ language_server/src/lsp/watchers.h
@@ -22,28 +22,6 @@
 
 namespace hlasm_plugin::language_server::lsp {
 
-enum class watch_kind
-{
-    _default = 0,
-    _create = 1,
-    _change = 2,
-    _delete = 4,
-};
-
-[[nodiscard]] constexpr watch_kind operator|(watch_kind l, watch_kind r) noexcept
-{
-    return static_cast<watch_kind>(static_cast<int>(l) | static_cast<int>(r));
-}
-
-struct watcher
-{
-    std::string_view pattern;
-    std::string_view base = {};
-    watch_kind kind = watch_kind::_default;
-};
-
-void to_json(nlohmann::json& j, const watcher& w);
-
 nlohmann::json watcher_registeration(parser_library::watcher_registration_id id, std::string_view uri, bool r);
 nlohmann::json watcher_unregisteration(parser_library::watcher_registration_id id);
 nlohmann::json default_watcher_registration();
