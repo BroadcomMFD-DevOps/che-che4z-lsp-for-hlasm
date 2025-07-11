634ba9277c561d6a611105b65982969291cc1877
Slavomir Kucera <slavomir.kucera@broadcom.com>
2025-07-11 15:23:41 +0200

    ...

---
+++ clients/vscode-hlasmplugin/src/test/suite/mfCreds.test.ts
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Broadcom, Inc. - initial API and implementation
+ */
+
+import * as assert from 'assert';
+import { getLastRunConfig } from "../../mfCreds";
+import type { ExtensionContext } from 'vscode';
+
+suite('MF Credentials', () => {
+
+    test('Get last run config', async () => {
+        const last = getLastRunConfig({
+            globalState: {
+                get() { return { host: 'h', user: 'u', jobcard: 'j' }; }
+            }
+        } as unknown as ExtensionContext);
+
+        assert.deepStrictEqual(last, { host: 'h', user: 'u', jobcard: 'j' });
+    });
+});
