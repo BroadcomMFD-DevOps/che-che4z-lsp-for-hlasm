b542f1f255ed4051f0eb3fb60f70e84934aeb2b0
Slavomir Kucera <slavomir.kucera@broadcom.com>
2025-11-20 15:24:19 +0100

    arithmetic tests

--- parser_library/test/processing/external_functions_test.cpp
+++ parser_library/test/processing/external_functions_test.cpp
@@ -19,22 +19,42 @@
 #include "../common_testing.h"
 #include "diagnostic.h"
 
+namespace {
+auto test_arithmetic_external_adder()
+{
+    return external_functions_list {
+        {
+            "ADD",
+            [](external_function_args& args) {
+                if (auto* aarg = args.arithmetic())
+                    for (auto x : aarg->args)
+                        aarg->result += x;
+            },
+        },
+    };
+}
+
+auto test_character_external_adder()
+{
+    return external_functions_list {
+        {
+            "ADD",
+            [](external_function_args& args) {
+                if (auto* aarg = args.character())
+                    for (auto x : aarg->args)
+                        aarg->result += x;
+            },
+        },
+    };
+}
+} // namespace
+
 TEST(external_functions, arithmetic)
 {
     std::string input(R"(
 &A  SETAF 'ADD',1,2
 )");
-    analyzer a(input,
-        analyzer_options(external_functions_list {
-            {
-                "ADD",
-                [](external_function_args& args) {
-                    if (auto* aarg = args.arithmetic())
-                        for (auto x : aarg->args)
-                            aarg->result += x;
-                },
-            },
-        }));
+    analyzer a(input, analyzer_options(test_arithmetic_external_adder()));
     a.analyze();
 
     EXPECT_TRUE(a.diags().empty());
@@ -49,17 +69,7 @@ TEST(external_functions, character)
     std::string input(R"(
 &A  SETCF 'ADD','A','B'
 )");
-    analyzer a(input,
-        analyzer_options(external_functions_list {
-            {
-                "ADD",
-                [](external_function_args& arg) {
-                    if (auto* carg = arg.character())
-                        for (auto x : carg->args)
-                            carg->result += x;
-                },
-            },
-        }));
+    analyzer a(input, analyzer_options(test_character_external_adder()));
     a.analyze();
 
     EXPECT_TRUE(a.diags().empty());
@@ -162,4 +172,111 @@ TEST(external_functions, function_not_a_string_3)
     a.analyze();
 
     EXPECT_TRUE(matches_message_codes(a.diags(), { "CE017", "CE017" }));
+}
+
+TEST(external_functions, arithmetic_wrong_args_1)
+{
+    std::string input(R"(
+&C  SETC  'A'
+&A  SETAF 'ADD',&C
+)");
+    analyzer a(input, analyzer_options(test_arithmetic_external_adder()));
+    a.analyze();
+
+    EXPECT_TRUE(matches_message_codes(a.diags(), { "CE012" }));
+}
+
+TEST(external_functions, arithmetic_wrong_args_2)
+{
+    std::string input(R"(
+&C  SETC  'A'
+&A  SETAF 'ADD','&C'
+)");
+    analyzer a(input, analyzer_options(test_arithmetic_external_adder()));
+    a.analyze();
+
+    EXPECT_TRUE(matches_message_codes(a.diags(), { "E013", "CE004" }));
+}
+
+TEST(external_functions, arithmetic_ord_arg)
+{
+    std::string input(R"(
+A   EQU   5
+&C  SETC  'A'
+&R  SETAF 'ADD',&C
+)");
+    analyzer a(input, analyzer_options(test_arithmetic_external_adder()));
+    a.analyze();
+
+    EXPECT_TRUE(a.diags().empty());
+
+    EXPECT_EQ(get_var_value<A_t>(a.hlasm_ctx(), "R"), 5);
+}
+
+TEST(external_functions, arithmetic_expr_1)
+{
+    std::string input(R"(
+&A  SETA  1
+&R  SETAF 'ADD',NOT &A
+)");
+    analyzer a(input, analyzer_options(test_arithmetic_external_adder()));
+    a.analyze();
+
+    EXPECT_TRUE(a.diags().empty());
+
+    EXPECT_EQ(get_var_value<A_t>(a.hlasm_ctx(), "R"), -2);
+}
+
+TEST(external_functions, arithmetic_expr_2)
+{
+    std::string input(R"(
+&A  SETA  1
+&R  SETAF 'ADD',&A+&A
+)");
+    analyzer a(input, analyzer_options(test_arithmetic_external_adder()));
+    a.analyze();
+
+    EXPECT_TRUE(a.diags().empty());
+
+    EXPECT_EQ(get_var_value<A_t>(a.hlasm_ctx(), "R"), 2);
+}
+
+TEST(external_functions, arithmetic_self_reference)
+{
+    std::string input(R"(
+&A  SETAF 'ADD',&A+&A
+)");
+    analyzer a(input, analyzer_options(test_arithmetic_external_adder()));
+    a.analyze();
+
+    EXPECT_TRUE(a.diags().empty());
+
+    EXPECT_EQ(get_var_value<A_t>(a.hlasm_ctx(), "A"), 0);
+}
+
+TEST(external_functions, arithmetic_array)
+{
+    std::string input(R"(
+&A(1) SETAF 'ADD',1,2
+)");
+    analyzer a(input, analyzer_options(test_arithmetic_external_adder()));
+    a.analyze();
+
+    EXPECT_TRUE(a.diags().empty());
+
+    EXPECT_EQ(get_var_vector<A_t>(a.hlasm_ctx(), "A"), std::vector(1, 3));
+}
+
+TEST(external_functions, arithmetic_boolean)
+{
+    std::string input(R"(
+&B  SETB  1
+&A  SETAF 'ADD',&B,&B
+)");
+    analyzer a(input, analyzer_options(test_arithmetic_external_adder()));
+    a.analyze();
+
+    EXPECT_TRUE(a.diags().empty());
+
+    EXPECT_EQ(get_var_value<A_t>(a.hlasm_ctx(), "A"), 2);
 }
