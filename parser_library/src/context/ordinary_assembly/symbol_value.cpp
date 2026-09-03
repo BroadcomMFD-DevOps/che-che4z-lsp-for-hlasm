10fdcbf87ac198276a8a5340a1d31196aa6e646a
Slavomir Kucera <slavomir.kucera@broadcom.com>
2026-09-03 12:33:04 +0200

    fix: Temporary fix for division by -1

--- parser_library/src/context/ordinary_assembly/symbol_value.cpp
+++ parser_library/src/context/ordinary_assembly/symbol_value.cpp
@@ -105,7 +105,7 @@ symbol_value symbol_value::operator*(const symbol_value& value) const
 symbol_value symbol_value::operator/(const symbol_value& value) const
 {
     if (value_kind() == symbol_value_kind::ABS && value.value_kind() == symbol_value_kind::ABS)
-        return value.get_abs() == 0 ? 0 : get_abs() / value.get_abs();
+        return value.get_abs() == 0 ? 0 : static_cast<abs_value_t>(static_cast<long long>(get_abs()) / value.get_abs());
 
     return symbol_value();
 }
